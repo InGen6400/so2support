ea51d6a79e773e7f163c88788daeb87a3302f9dc
sugar6400 <sugar6400@gmail.com>
2018-10-01 12:04:59 +0900

    first commit

---
+++ app/src/main/java/sugar6400/github/io/so2support/CalcActivity.java
@@ -0,0 +1,13 @@
+package sugar6400.github.io.so2support;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}

6ac0bf91e2658929eb67de060c1c2b5dbd39be83
Sugar Light <sugar6400@gmail.com>
2018-11-30 13:32:02 +0900

    平均値が0のときにエラーが出ないよう修正

--- app/src/main/java/sugar6400/github/io/so2support/datas/ReceiveItem.java
+++ app/src/main/java/sugar6400/github/io/so2support/datas/ReceiveItem.java
@@ -11,11 +11,21 @@ public class ReceiveItem {
 
     @SuppressWarnings("unchecked")
     ReceiveItem(Map<String, Object> data) {
-        cheap5_day = (double) data.get("cheap5_day_ave");
-        cheap5_week = (double) data.get("cheap5_week_ave");
+        Object recv = data.get("cheap5_day_ave");
+        if(recv instanceof Double) {
+            cheap5_day = (double) recv;
+        }else{
+            cheap5_day = (long) recv;
+        }
+        recv = data.get("cheap5_week_ave");
+        if(recv instanceof Double){
+            cheap5_week = (double) recv;
+        }else{
+            cheap5_week = (long) recv;
+        }
         cheapest = new ArrayList<>();
         for (Object sale : (ArrayList<Object>) data.get("cheapest_now")) {
-            cheapest.add((SaleData) sale);
+            cheapest.add(new SaleData((Map<String, Object>) sale));
         }
     }
 }
